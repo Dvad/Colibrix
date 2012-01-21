fe1db992b7177bf220512ebaf50466adff5a6915
DavidCaruso <dav@myhost>
2012-01-22 00:42:35 +0100

    Rajout d'une fonction pour tester la présence de données Sonar; GotSonarData. Renvoie 0 si pas de données.

--- src/i2C.c
+++ src/i2C.c
@@ -3,7 +3,7 @@
  *
  *  Created on: 19 janv. 2012
  *      Author: dav
- *      Ces fonctions gère les périphérique commandés en I2C et initialise le port I2C
+ *      Ces fonctions gèrent les périphériques commandés en I2C et initialise le port I2C
  */
 #include <stdint.h>
 #include <sys/types.h>
@@ -87,6 +87,9 @@ int I2C_Envoyer_Commande_Tout_Moteur(unsigned char commande[4]){
 	return 0;
 }
 
+//
+//Envoie le début de la lecture du sonar. Attention
+//
 int I2C_Envoyer_Pulse_Sonar(){
 	ioctl(fdc,I2C_SLAVE, 0xE0);
 	commandePulseSonar[0]=0x55; //Pour une réponse en cm voir datasheet
@@ -96,8 +99,40 @@ int I2C_Envoyer_Pulse_Sonar(){
 	return 0;
 }
 
-int I2C_Envoyer_Lecture_Sonar(){
+//
+//Envoie une tentative de lecture sur le sonar, si celui si n'a pas fini la mesure il renvoie 255 255 (normalemen
+//On renvoie 0 dans ce cas, 1 sinon;
+//
+int GotSonarData(){
+	ioctl(fdc,I2C_SLAVE, 0xE0);
+	wbuf[0] = 0x00;//Register
+	wbuf[1] = 0x01;//byte to read
+	m[0].len = 2;//2 byte write
+	m[0].flags = 0;//prepare to write
+	m[0].addr = 0xE0;//slave addr
+	m[0].buf = wbuf;//write buffer
 
+	m[1].len = 1;//1 byte read
+	m[1].flags = I2C_M_RD;//prepare to read
+	m[1].addr = 0xE0;//slave addr
+	m[1].buf = rbuf;//read buffer
+
+	rdwr.msgs= m;//first msg
+	rdwr.nmsgs=2;// number of msgs
+
+	if (ioctl(fdc,I2C_RDWR,&rdwr) < 0) {
+		printf("TestDonnéeSonar:Erreur de lecture.\n");
+		return -1;
+	}
+	if (rbuf==255)return 0;
+	else return 1;
+
+}
+
+	//
+	//Renvoie l'entier représentant la dernière lecture Sonar, -1 en cas d'erreur de lecture,-2 en cas de mesure non terminé, il faut alors continuer de tenter de lire une valeur correcte.
+	//
+int I2C_Envoyer_Lecture_Sonar(){
 
 	wbuf[0] = 0x02;//Register
 	wbuf[1] = 0x02;//byte to read
@@ -117,6 +152,9 @@ int I2C_Envoyer_Lecture_Sonar(){
 	if (ioctl(fdc,I2C_RDWR,&rdwr) < 0) {
 		printf("Ne peut pas lire les données sonar\n");
 		return -1;
+	}
+	else{
+		if (rbuf[0]==255 && rbuf[1]==255) return -2;
 	}
 	return (int)((int)rbuf[0]*255+(int)rbuf[1]);
 }
