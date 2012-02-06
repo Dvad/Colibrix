60bc7eaf5da09657447b3f3f1e6f99ea2f4e5936
dav <dav@localhost.localdomain>
2012-02-06 18:05:54 +0100

    Modification des fonction d'ordre sonar après mesure à l'oscilloscope.

--- src/i2C.c
+++ src/i2C.c
@@ -13,14 +13,15 @@
 #include <linux/i2c.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/ioctl.h> //pour x86
 #include "i2C.h"
 #include "Params.h"
 
 int fdc = -1;
 
-unsigned char commandePulseSonar[3];
-unsigned char commandeLectureSonar[3];
+unsigned char commandePulseSonar[5];
+unsigned char commandeLectureSonar[5];
 
 struct i2c_msg m[2];
 struct i2c_rdwr_ioctl_data rdwr;
@@ -32,7 +33,7 @@ int I2C_Initialise(){
 	printf("Ouverture de l'interface I2C...");
 	fdc = open("/dev/i2c-3", O_RDWR); // | O_NONBLOCK); // | O_NOCTTY | O_NDELAY);
 	if (fdc < 0) {
-		printf("\nERREUR: open() é renvoyé %i !\n", fdc);
+		printf("\nERREUR: open() à renvoyé %i !\n", fdc);
 		return fdc;
 	} else {
 		printf(" OK\n");
@@ -45,8 +46,8 @@ int I2C_Envoyer_Commande_Moteur(int moteur, unsigned char commande[1]){
 
 	switch(moteur){
 	case 1:{
-		ioctl(fdc, I2C_SLAVE, 0x52);
-		write(fdc, commande[0], 1);
+		ioctl(fdc, I2C_SLAVE, 0x52); //TODO vérifier que l'adresse est correctement écrite, la gumst a tendance à inverser les bit lors de l'envoi ex
+		write(fdc, commande[0], 1);  // 0xE0=0b1110 0000 envoi en réalité 0x70=0b0111 0000
 		break;
 	}
 	case 2:{
@@ -92,23 +93,28 @@ int I2C_Envoyer_Commande_Tout_Moteur(unsigned char commande[4]){
 //Envoie le début de la lecture du sonar. Attention
 //
 int I2C_Envoyer_Pulse_Sonar(){
-	ioctl(fdc,I2C_SLAVE, 0xE0);
-	commandePulseSonar[0]=0x55; //Pour une réponse en cm voir datasheet
-	commandePulseSonar[1]=GAIN_MAX_SONAR;
-	commandePulseSonar[2]=RANGE_SONAR_REGISTER;
-	write(fdc,commandePulseSonar,3);
-	return 0;
+	ioctl(fdc,I2C_SLAVE, 0x70);
+	commandePulseSonar[0]=0x0; //registre
+	commandePulseSonar[1]=0x55; //Pour une réponse en cm voir datasheet
+	commandePulseSonar[2]=GAIN_MAX_SONAR;
+	commandePulseSonar[3]=RANGE_SONAR_REGISTER;
+	ret=write(fdc,commandePulseSonar,4);
+	if (ret==4)return 0 ;
+	else{
+		printf("Erreur communication Sonar : %s",strerror(errno));
+		return ret;
+	}
+
 }
 
 //
-//Envoie une tentative de lecture sur le sonar, si celui si n'a pas fini la mesure il renvoie 255 255 (normalemen
+//Envoie une tentative de lecture sur le sonar, si celui si n'a pas fini la mesure il renvoie 255 ou bien fait une erreur
 //On renvoie 0 dans ce cas, 1 sinon;
 //
 int GotSonarData(){
 	ioctl(fdc,I2C_SLAVE, 0xE0);
 	wbuf[0] = 0x00;//Register
-	wbuf[1] = 0x01;//byte to read
-	m[0].len = 2;//2 byte write
+	m[0].len = 1;//2 byte write
 	m[0].flags = 0;//prepare to write
 	m[0].addr = 0xE0;//slave addr
 	m[0].buf = wbuf;//write buffer
@@ -122,8 +128,8 @@ int GotSonarData(){
 	rdwr.nmsgs=2;// number of msgs
 
 	if (ioctl(fdc,I2C_RDWR,&rdwr) < 0) {
-		printf("TestDonnéeSonar:Erreur de lecture.\n");
-		return -1;
+		printf("ErreutTestDonnéeSonar.\n");
+		return 0;
 	}
 	if (rbuf[0]==255)return 0;
 	else return 1;
@@ -134,18 +140,24 @@ int GotSonarData(){
 	//Renvoie l'entier représentant la dernière lecture Sonar, -1 en cas d'erreur de lecture,
     //-2 en cas de mesure non terminé, il faut alors continuer de tenter de lire une valeur correcte.
 	//
+	//Ici on n'utilise une fonction du driver i2C qui permet d'initier l'écrture (Pour sélectionner le registre voulu)
+	//En même temps que la lecture (Pour lire à partir de ce registre dans la mémoire du sonar)
+	// Cela règle les problème de conflit éventuel avec les vario i2C sur le même bus.
 int I2C_Envoyer_Lecture_Sonar(){
-
-	wbuf[0] = 0x02;//Register
-	wbuf[1] = 0x02;//byte to read
-	m[0].len = 2;//2 byte write
-	m[0].flags = 0;//prepare to write
-	m[0].addr = 0xE0;//slave addr
+	//
+	//On écrit pour sélectionner le registre
+	//
+	wbuf[0] = 0x02	;//Registre qui servira de début de lecture!
+	m[0].len = 1;//2 byte write
+	m[0].flags = 0;
+	m[0].addr = 0x70;//slave addr
 	m[0].buf = wbuf;//write buffer
-
+	//
+	//On lit m[1].len byte à partir du registre
+	//
 	m[1].len = 2;//1 byte read
 	m[1].flags = I2C_M_RD;//prepare to read
-	m[1].addr = 0xE0;//slave addr
+	m[1].addr = 0x70;//slave addr
 	m[1].buf = rbuf;//read buffer
 
 	rdwr.msgs= m;//first msg
@@ -153,10 +165,6 @@ int I2C_Envoyer_Lecture_Sonar(){
 
 	if (ioctl(fdc,I2C_RDWR,&rdwr) < 0) {
 		printf("Ne peut pas lire les données sonar\n");
-		return -1;
-	}
-	else{
-		if (rbuf[0]==255 && rbuf[1]==255) return -2;
 	}
 	return (int)((int)rbuf[0]*255+(int)rbuf[1]);
 }
